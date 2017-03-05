5e4cb4204d93b0435e0c269c36cf9498281c1111
Charlie Leathers <cleathers89@gmail.com>
2017-03-04 21:05:34 -0800

    registers todos routes to todo router file

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/todos', todos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
